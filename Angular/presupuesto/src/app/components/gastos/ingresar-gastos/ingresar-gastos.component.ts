45b8eb507b3e82fd7c14a8d925be6308d9557184
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-06 11:45:36 -0300

    agregado de mensaje de error en caso de que los datos ingresados a gastos/ingresar-gastos sean incorrectos

--- Angular/presupuesto/src/app/components/gastos/ingresar-gastos/ingresar-gastos.component.ts
+++ Angular/presupuesto/src/app/components/gastos/ingresar-gastos/ingresar-gastos.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ingresar-gastos',
   templateUrl: './ingresar-gastos.component.html',
-  styleUrls: ['./ingresar-gastos.component.css']
+  styleUrls: ['./ingresar-gastos.component.css'],
 })
 export class IngresarGastosComponent {
+  nombreGasto: string;
+  cantidad: number;
+  formularioIncorrecto: boolean;
+  textIncorrecto: string;
 
+  constructor() {
+    this.nombreGasto = '';
+    this.cantidad = 0;
+    this.formularioIncorrecto = false;
+    this.textIncorrecto = 'Nombre de gasto o cantidad incorrecta';
+  }
+
+  ngOnInit(): void {}
+
+  agregarGasto(){
+    if (this.nombreGasto === '' || this.cantidad <= 0) {
+      this.formularioIncorrecto = true;
+    } else {
+      this.formularioIncorrecto = false;
+      // Aquí se debe agregar el gasto
+      this.nombreGasto = '';
+      this.cantidad = 0;
+    }
+  }
 }
