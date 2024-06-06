08607473afc727bc95d7881615dd4dc8317e4ea7
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-06 10:14:46 -0300

    creacion de componentes para "presupuesto"

--- Angular/presupuesto/src/app/app.module.ts
+++ Angular/presupuesto/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
+import { IngresarGastosComponent } from './components/gastos/ingresar-gastos/ingresar-gastos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarPresupuestoComponent,
+    ListarGastoComponent,
+    IngresarGastosComponent
   ],
   imports: [
     BrowserModule,
