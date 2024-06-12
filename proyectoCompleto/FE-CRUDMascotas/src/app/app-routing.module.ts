69d22585a41250a1885f60e82b476fdb30f209bd
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-12 09:57:46 -0300

    agregado de rutas a los botones

--- proyectoCompleto/FE-CRUDMascotas/src/app/app-routing.module.ts
+++ proyectoCompleto/FE-CRUDMascotas/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoMascotasComponent } from './components/listado-mascotas/listado-mascotas.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listadoMascotas', pathMatch: 'full' },
+  { path: 'listadoMascotas', component: ListadoMascotasComponent },
+  { path: 'agregarMascota', component: AgregarEditarMascotaComponent },
+  { path: 'verMascota/:id', component: VerMascotaComponent },
+  { path: 'editarMascota/:id', component: AgregarEditarMascotaComponent },
+  { path: '**', redirectTo: 'listadoMascotas', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
