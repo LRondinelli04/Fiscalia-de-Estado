88885d32a6a71db0fc9f72dcc79f5428aa73094d
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-14 12:42:05 -0300

    BE-CRUDMascotas Completo

---
+++ proyectoCompleto/BE-CRUDMascotas/Migrations/20240613134303_initial1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BE_CRUDMascotas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial1 : Migration // Corrección del nombre de la clase
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Verifica si ya existe una implementación previa de este método en otra parte del código.
+            // Si es así, considera cambiar el nombre de esta clase de migración o fusionar las implementaciones.
+            migrationBuilder.CreateTable(
+                name: "Mascotas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Raza = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Edad = table.Column<int>(type: "int", nullable: false),
+                    Peso = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascotas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Verifica si ya existe una implementación previa de este método en otra parte del código.
+            // Si es así, considera cambiar el nombre de esta clase de migración o fusionar las implementaciones.
+            migrationBuilder.DropTable(
+                name: "Mascotas");
+        }
+    }
+}
---
+++ proyectoCompleto/BE-CRUDMascotas/Models/Repository/MascotaRepository.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace BE_CRUDMascotas.Models.Repository
+{
+    public class MascotaRepository: IMascotaRepository
+    {
+        private readonly AplicationDbContext _context;
+
+        public MascotaRepository(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<List<Mascota>> GetListMascotas()
+        {
+            return await _context.Mascotas.ToListAsync();
+        }
+
+        public async Task<Mascota> GetMascota(int id)
+        {
+            return await _context.Mascotas.FindAsync(id);
+        }
+        public async Task DeleteMascota(Mascota mascota)
+        {
+            // Eliminar la mascota
+            _context.Mascotas.Remove(mascota);
+            // Guardar los cambios
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Mascota> AddMascota(Mascota mascota)
+        {
+            // Agregar una mascota
+            _context.Mascotas.Add(mascota);
+            // Guardar los cambios
+            await _context.SaveChangesAsync();
+            return mascota;
+        }
+
+        public async Task UpdateMascota(Mascota mascota)
+        {
+            var mascotaItem = await _context.Mascotas.FirstOrDefaultAsync(x => x.Id == mascota.Id);
+
+            if (mascotaItem == null)
+            { 
+                // Si encuentra la mascota, actualiza los campos
+                mascotaItem.Nombre = mascota.Nombre;
+                mascotaItem.Edad = mascota.Edad;
+                mascotaItem.Peso = mascota.Peso;
+                mascotaItem.Raza = mascota.Raza;
+                mascotaItem.Color = mascota.Color;
+
+                // Guardar los cambios
+                await _context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
