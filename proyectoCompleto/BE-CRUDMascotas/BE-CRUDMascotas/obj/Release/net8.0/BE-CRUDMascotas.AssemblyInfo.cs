eeb1129363fb8e73dc278a016521519fcf64e719
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-18 09:46:35 -0300

    apertura del proyecto a traves de la carpeta del repo

--- proyectoCompleto/BE-CRUDMascotas/BE-CRUDMascotas/obj/Release/net8.0/BE-CRUDMascotas.AssemblyInfo.cs
+++ proyectoCompleto/BE-CRUDMascotas/BE-CRUDMascotas/obj/Release/net8.0/BE-CRUDMascotas.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE-CRUDMascotas")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d0f93e95a91b33dc735df69d91efd45e485f7d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE-CRUDMascotas")]
