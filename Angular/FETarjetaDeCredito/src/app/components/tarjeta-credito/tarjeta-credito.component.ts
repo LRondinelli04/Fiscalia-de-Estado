774c1280719675fbfcd81e1bc05c7f6642cce500
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-18 11:56:32 -0300

    configuracion del formulario para agregar una tarjeta de credito

--- Angular/FETarjetaDeCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ Angular/FETarjetaDeCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -28,7 +29,45 @@ export class TarjetaCreditoComponent {
     },
   ];
 
-  constructor() {}
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(16),
+          Validators.minLength(16),
+        ],
+      ],
+      fechaExpiracion: [
+        '',
+        [Validators.required, Validators.maxLength(5), Validators.minLength(5)],
+      ],
+      cvv: [
+        '',
+        [Validators.required, Validators.maxLength(3), Validators.minLength(3)],
+      ],
+    });
+  }
 
   ngOnInit(): void {}
+
+  agregarTarjeta() {
+    // Guardo los valores ingresados en el formulario en la constante tarjeta
+    const tarjeta: any = {
+      titular: this.form.value.titular,
+      numeroTarjeta: this.form.value.numeroTarjeta,
+      fechaExpiracion: this.form.value.fechaExpiracion,
+      cvv: this.form.value.cvv,
+    };
+
+    // Agrego la tarjeta a la lista de tarjetas
+    this.listTarjetas.push(tarjeta);
+
+    // Reseteo el formulario
+    this.form.reset();
+  }
 }
