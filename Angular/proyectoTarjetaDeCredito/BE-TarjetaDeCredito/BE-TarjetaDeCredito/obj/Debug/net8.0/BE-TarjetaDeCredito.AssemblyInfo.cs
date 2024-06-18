933879592c07d12bc14b936c9903611cff0152ec
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-18 12:54:05 -0300

    agregado de models TarjetaCredito

--- Angular/proyectoTarjetaDeCredito/BE-TarjetaDeCredito/BE-TarjetaDeCredito/obj/Debug/net8.0/BE-TarjetaDeCredito.AssemblyInfo.cs
+++ Angular/proyectoTarjetaDeCredito/BE-TarjetaDeCredito/BE-TarjetaDeCredito/obj/Debug/net8.0/BE-TarjetaDeCredito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE-TarjetaDeCredito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05dad43c9e6d74e8846020bb206d777d362ca75f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468c2f6a20e0998581943d684114e9b062e99a7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE-TarjetaDeCredito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE-TarjetaDeCredito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
