2fd5186e4d86c38733d7eaa22dbbf36ccbbf5971
LRondinelli04 <lucasrondinelli04@gmail.com>
2024-06-18 10:49:46 -0300

    agregado de listado de tarjetas en base a lo que contenga el component.ts

--- Angular/FETarjetaDeCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ Angular/FETarjetaDeCredito/src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tarjeta-credito',
   templateUrl: './tarjeta-credito.component.html',
-  styleUrls: ['./tarjeta-credito.component.css']
+  styleUrls: ['./tarjeta-credito.component.css'],
 })
 export class TarjetaCreditoComponent {
+  // Variables
+  listTarjetas: any[] = [
+    {
+      titular: 'Juan Perez',
+      numeroTarjeta: '1234 1234 1234 1234',
+      fechaExpiracion: '01/23',
+      cvv: '123',
+    },
+    {
+      titular: 'Maria Rodriguez',
+      numeroTarjeta: '5678 567 5678 5678',
+      fechaExpiracion: '01/23',
+      cvv: '123',
+    },
+    {
+      titular: 'Pedro Gomez',
+      numeroTarjeta: '9876 9876 9876 9876',
+      fechaExpiracion: '01/23',
+      cvv: '123',
+    },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
